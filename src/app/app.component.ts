3dd0183afb1c34ee438770eb9b20595fee44c0f0
yuri <yuri-simoes@hotmail.com>
2019-08-09 22:54:31 -0300

    ir work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'img-upload';
+
+  public imagePath;
+  imgURL: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  inputFileChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      const picture = event.target.files[0];
+
+      const formData = new FormData();
+      formData.append('foto', picture);
+
+      this.http.post('http://localhost:8080/fotos', formData)
+        .subscribe(Response => console.log(Response, 'it works.'));
+
+      const reader = new FileReader();
+      this.imagePath = event.target.files;
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = showIgm => {
+        this.imgURL = reader.result;
+      };
+    }
+  }
 }
